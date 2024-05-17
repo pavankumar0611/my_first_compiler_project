6240583f8fab08ac3837c3c6d9fc76f641260d06
pavankumar <pavankohli0644@gmail.com>
2024-05-17 16:13:49 +0530

    how parser recognises the grammer

---
+++ 02_Parser/main.c
@@ -0,0 +1,43 @@
+#include "defs.h"
+#define extern_
+#include "data.h"
+#undef extern_
+#include "decl.h"
+#include <errno.h>
+
+
+
+static void init ()
+{
+	Line = 1;
+	Putback = '\n';
+}
+
+static void usage ( char *prog)
+{
+	fprintf (stderr , "usage : %s infile\n" , prog);
+	exit(1);
+}
+
+
+void main( int argc , char *argv[])
+{
+	struct ASTnode *n;
+
+	if( argc != 2)
+		usage (argv [0]);
+
+	init();
+
+	if ( (Infile = fopen ( argv [1] , "r" )) == NULL )
+	{
+		fprintf( stderr , "unable to open %s : %s\n" , argv[1] , strerror (errno));
+		exit(1);
+	}
+
+	scan (&Token );
+	n = binexpr();
+	printf("%d\n", interpretAST (n));
+	exit(0);
+}
+
---
+++ 02_Parser/tree.c
@@ -0,0 +1,35 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+struct ASTnode *mkastnode (int op , struct ASTnode *left , struct ASTnode *right , int intvalue)
+{
+	struct ASTnode *n;
+
+	n = (struct ASTnode *) malloc (sizeof(struct ASTnode));
+	if(n == NULL)
+	{
+		fprintf(stderr , "unable to malloc in mkastnode()\n");
+		exit(1);
+	}
+
+	n ->op = op;
+	n ->left = left;
+	n ->right = right;
+	n ->intvalue = intvalue;
+	return (n);
+}
+
+
+// make as AST leaf node
+struct ASTnode *mkastleaf (int op , int intvalue)
+{
+	return (mkastnode ( op , NULL , NULL , intvalue ));
+}
+
+
+struct ASTnode *mkastunary (int op , struct ASTnode *left , int value)
+{
+	return ( mkastnode ( op , left , NULL , value));
+}
+
