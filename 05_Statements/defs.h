fac9340082168482d1e034788b6d39322fadb314
pavankumar <pavankohli0644@gmail.com>
2024-05-17 16:29:27 +0530

    adding small statemnts to grammer

---
+++ 05_Statements/defs.h
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#define TEXTLEN 512
+
+enum {
+	T_EOF, T_PLUS , T_MINUS , T_STAR , T_SLASH , T_INTLIT , T_SEMI, T_PRINT
+};
+
+
+struct token {
+	int token;
+	int intvalue;
+};
+
+enum {
+	A_ADD, A_SUBTRACT , A_MULTIPLY, A_DIVIDE , A_INTLIT
+};
+
+struct ASTnode {
+	int op;
+	struct ASTnode *left;
+	struct ASTnode *right;
+	int intvalue;
+};
+
+
