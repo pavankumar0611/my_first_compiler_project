30f8447320b9aadf4ec5394e8ff3b3de2f9bc2a8
pavankumar0611 <pavankohli0644@gmail.com>
2024-09-01 16:48:55 +0530

    Using stack for local variable and how 16 bit  alignment are maintained for functions.

---
+++ 23_Local_Variables/tests/input33.c
@@ -0,0 +1,18 @@
+void main() {
+        printint(5 && 5);
+        printint(-5 && 8);
+        printint(-0 && -0);
+        printint(22 && -0);
+        printint(0 && 0);
+
+        int a, b;
+        a = 5;
+        b = -5;
+
+        while(a || b) {
+                printint(5);
+                a = a -1;
+                b = b + 1;
+        }
+
+}
