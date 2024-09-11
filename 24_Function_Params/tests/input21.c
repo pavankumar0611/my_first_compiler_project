d36deeb7a7e9e0415b0fa5a64d519a8b467ff39e
pavankumar0611 <pavankohli0644@gmail.com>
2024-09-11 22:19:06 +0530

    Implemented  the copying of function parameters out of registers and onto the fucntion stack.

---
+++ 24_Function_Params/tests/input21.c
@@ -0,0 +1,15 @@
+void main()
+{
+        int a [3];
+        int b ;int c; int d ; int e; int f;
+        a[1] = 200;
+        a[0] = 100;
+
+        b = a[0];
+        c = a[1];
+        printint(b);
+        printint(c);
+
+
+
+}
