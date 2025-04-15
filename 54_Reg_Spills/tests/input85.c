e6e6bc84d1148e95a5c66893417216088de7af5a
pavankumar0611 <pavankohli0644@gmail.com>
2025-04-14 18:44:10 -0700

    Part 54 & 55 : Spilling Registers
    Implemented register spill, why this order matters
    1. spill the registers into stack
    2. copying the function arguements into registers.
    3. func call
    4. Unspill the registers
    5. copy the func return value into registers.
    
    Implemented Lazy evaluation for
    && and || operators.

---
+++ 54_Reg_Spills/tests/input85.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+        int x;
+        switch (x)
+        {
+                case 1:
+                case 2:
+                default:
+                        x = 3;
+        }
+}
